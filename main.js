006af14b2943c96fe0408e9d65f77db5be1c06d9
Dilek Baykara <baykara.d@gmail.com>
2022-07-09 13:23:21 -0400

    Added operate function

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ const add = function(a, b) {
         : 0;
   };
   
+const divide = function(a,b) {
+return a / b;
+
+};
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate(num1, num2, operator) {
+    switch (operator) {
+      case "+":
+        return add(num1, num2);
+      case "-":
+        return subtract(num1, num2);
+      case "*":
+        return multiply(num1, num2);
+      case "/":
+        return divide(num1, num2);
+    }
+  };
